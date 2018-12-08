0a53505f954f48247b3e80b47dfe75787776de43
dqvidqs <dqvidqs@gmail.com>
2018-12-08 17:11:34 +0200

    Add files via upload

--- IS_CS/Global.asax.cs
+++ IS_CS/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,7 @@ namespace IS_CS
     {
         protected void Application_Start()
         {
+            Database.SetInitializer <IS_CS.Models.prekeContext>(null);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
